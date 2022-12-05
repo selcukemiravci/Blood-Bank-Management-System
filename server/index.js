dd0b1ef098f86cf2b446ba1bcd259913ab699af1
rei-tsunemi <reren1031@gmail.com>
2022-12-05 05:23:09 -0700

    login basics done 😭😭😭😭

--- server/index.js
+++ server/index.js
@@ -7,75 +7,50 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  user: "root",
-  host: "localhost",
-  password: "Password",
-  database: "bloodybank",
+    user: "root",
+    host: "localhost",
+    password: "Password",
+    database: "bloodybank",
 });
 
 app.post("/create", (req, res) => {
-  console.log(req.body);
-  const email = req.body.email;
-  const password = req.body.password;
+    console.log(req.body);
+    const email = req.body.email;
+    const password = req.body.password;
 
-  const healthcard = req.body.healthcard;
-  const firstname = req.body.firstname;
-  const lastname = req.body.lastname;
-  const age = req.body.age;
-  const address = req.body.address;
-  const bloodtype = req.body.bloodtype;
-  const gender = req.body.gender;
-  const reason = req.body.reason;
-  const phone = req.body.phone;
-
-  db.query(
-    "INSERT INTO registration (email, password) VALUES (?,?)",
-    [email, password],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send("Registration values are inserted!");
-      }
-    }
-  );
+    db.query(
+        "INSERT INTO registration (email, password) VALUES (?,?)", [email, password],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send("Registration values are inserted!");
+            }
+        }
+    );
 });
 
-app.post("/reqcall", (req, res) => {
-  console.log(req.body);
-  const healthcard = req.body.healthcard;
-  const firstname = req.body.firstname;
-  const lastname = req.body.lastname;
-  const age = req.body.age;
-  const address = req.body.address;
-  const bloodtype = req.body.bloodtype;
-  const gender = req.body.gender;
-  const reason = req.body.reason;
-  const phone = req.body.phone;
+app.post('/processLogin', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
-  db.query(
-    "INSERT INTO request (healthcard, firstname,lastname,age,address,bloodtype,gender,reason,phone) VALUES (?,?,?,?,?,?,?,?,?)",
-    [
-      healthcard,
-      firstname,
-      lastname,
-      age,
-      address,
-      bloodtype,
-      gender,
-      reason,
-      phone,
-    ],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send("Request values are inserted!");
-      }
-    }
-  );
+    db.query(
+        "SELECT * FROM registration WHERE email = ? AND password = ?", [email, password],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            if (result.length > 0) {
+                res.send(result);
+            } else {
+                res.send("Wrong email or password combination!!")
+            }
+        }
+    );
 });
+
+
 
 app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+    console.log("Server is running on port 3001");
 });
